e35ba638159321d1fc80eb1c090f40155c2d2769
Manousos P. Koutsoukos <m.p.koutsoukos@gmail.com>
2017-03-30 01:56:59 +0300

    Added solution to project euler problem 2

--- ProjectEulerWithHaskell.hs
+++ ProjectEulerWithHaskell.hs
@@ -2,3 +2,9 @@
 
 project_euler_1 = sum $ map (\x -> if x `mod` 3 ==0 || x `mod` 5 ==0 then x else 0) [1..999]
 -- 233168
+
+--Project Euler problem 2 : Sum of even valued fibonacci numbers under 4000000
+
+fib = 0 : 1 : zipWith (+) fib (tail fib)
+
+project_euler_2 = sum [x | x <- takeWhile(<= 4000000) fib, x `mod` 2 ==0]
